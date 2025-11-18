c4c2a4b68f8533fe58a3f74080208caf1e2e8b7c
Gonzalo0614 <118952172+Gonzalo0614@users.noreply.github.com>
2025-11-18 20:55:21 -0300

    CommitMaestro

---
+++ src/ar/edu/unlam/pb2/elandria/FaltaDeMaestriaException.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.pb2.elandria;
+
+public class FaltaDeMaestriaException extends Exception {
+
+}
---
+++ src/ar/edu/unlam/pb2/elandria/MaestriaInsuficienteException.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.pb2.elandria;
+
+public class MaestriaInsuficienteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MaestriaInsuficienteException(String mensaje) {
+		super(mensaje);
+	}
+
+}
